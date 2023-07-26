165988fbac5108f47966a06f57c2ccf636730a6e
shirazto24-meet <139688798+shirazto24-meet@users.noreply.github.com>
2023-07-26 22:23:14 +0300

    SIGNING UP IS USER INTERACTIVITY!!!

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -11,7 +11,7 @@ config = {
   'storageBucket': "spotify-8be88.appspot.com",
   'messagingSenderId': "594905802853",
   'appId': "1:594905802853:web:6dba497a68e103b24f7e8c",
-  "databaseURL":""
+  "databaseURL":"https://spotify-8be88-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 #Code goes below here
@@ -23,6 +23,10 @@ db = firebase.database()
 def  home():
     return render_template('index.html')
 
+@app.route("/main")
+def main():
+    return render_template('main.html')
+
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
@@ -32,7 +36,7 @@ def signin():
         password = request.form['password']
         try:
             login_session['user'] = auth.sign_in_with_email_and_password(email,password)
-            return redirect(url_for('home'))
+            return redirect(url_for('main'))
         except:
             error = 'signin failed'
             return render_template("signin.html")
@@ -49,18 +53,16 @@ def signup():
         full_name = request.form['full_name']
         username = request.form['username']
 
-        
+        # try:
+        login_session['user'] = auth.create_user_with_email_and_password(email, password)
 
-        try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
-
-            UID = login_session['user']['localId']
-            user = {"email":email,"password":password,"full_name":full_name,"username": username }
-            db.child("Users").child(UID).set(user)
-            return redirect(url_for('signin'))
-        except:
-            error = "auth failed"
-            return render_template("signup.html")
+        UID = login_session['user']['localId']
+        user = {"email":email,"password":password,"full_name":full_name,"username": username }
+        db.child("Users").child(UID).set(user)
+        return redirect(url_for('signin'))
+        # except:
+        #      error = "auth failed"
+        #      return render_template("signup.html")
     else:
         return render_template('signup.html')
 
